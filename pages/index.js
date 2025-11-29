57a7a88e3afb1c01bdff919743c29714ad6bc13c
Waibai-H <henrybr090@gmail.com>
2025-11-29 03:10:10 +0000

    Modifica frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Maria Paula, te amo, se me ama, dá uma risadinha!</h1>
+    return <h1>Maria Paula, te amo, se me ama, dá uma risadinha! HAHUI</h1>
 }
 
 export default Home;
