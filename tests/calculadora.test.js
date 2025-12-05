dba39312c6a65fef482551fc65b31ac8a0eb8f9b
Adrian Henry <henrybr090@gmail.com>
2025-12-05 00:50:32 +0000

    Adiciona arquivos de teste

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,16 @@
-test("Espero 1, sendo 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deve retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 100 + 5 deve retornar 105", () => {
+  const resultado = calculadora.somar(100, 5);
+  expect(resultado).toBe(105);
+});
+
+test("somar banana + 100 deve retorna Erro", () => {
+  const resultado = calculadora.somar("banana", "100");
+  expect(resultado).toBe("Erro");
 });
