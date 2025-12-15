02b1c6e88818d64e3bf33584fe1a9f066e84c9bf
Adrian Henry <henrybr090@gmail.com>
2025-12-15 20:14:09 +0000

    Adicionando max & open_connections para retorno no /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,11 +6,25 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const dataBaseVersionValue = databaseVersionResult.rows[0].server_version;
 
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
+
+  const dataBaseOpenConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const databaseOpenConnectionsValue =
+    dataBaseOpenConnectionsResult.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        open_connections: databaseOpenConnectionsValue,
       },
     },
   });
