e8e8bc9a90595494e5543101d1edbd18f2dbcb96
Adrian Henry <henrybr090@gmail.com>
2026-07-16 12:50:13 +0000

    adds `/api/v1/migrations` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
-test("Get to /api/v1/status should return 200", async () => {
+import database from "infra/database";
+
+beforeAll(CleanDatabase);
+
+async function CleanDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
